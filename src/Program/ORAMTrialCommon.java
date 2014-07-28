1e4a4169f2256d256c8ae35b0b8568745a2f022b
Boyoung- <byaweics@gmail.com>
2014-07-28 14:53:33 -0700

    attempt to set sigmas to be ramdom, but don't know why it causes bad label exception

--- src/Program/ORAMTrialCommon.java
+++ src/Program/ORAMTrialCommon.java
@@ -3,6 +3,7 @@
 package Program;
 
 import java.math.*;
+import java.util.Random;
 
 import Utils.*;
 import YaoGC.*;
@@ -12,22 +13,38 @@ public class ORAMTrialCommon extends ProgCommon {
     static int cBitLen;
 
     public static String circuit = null;
+    private static Random rand = new Random();
 
     static int bitLength(int x) {
     	return BigInteger.valueOf(x).bitLength();
     }
 
+    private static int randInt(int min, int max) {
+	int randomNum = rand.nextInt((max - min) + 1) + min;
+	return randomNum;
+    }
+
     protected static void initCircuits() {
-	int s1, s2;
+	// TODO: figure out why using random numbers for s1 and s2
+	//       causes bad label exception
+	int s1, s2, w, tmp1, tmp2, min, max;
+	if (circuit.equals("F2ET"))
+	    w = sBitLen+cBitLen-2;
+	else
+	    w = (sBitLen+cBitLen-2)/2;	
+	tmp1 = randInt(1, w);
+	tmp2 = randInt(1, w);
+	//s1 = Math.min(tmp1, tmp2);
+	//s2 = Math.max(tmp1, tmp2);
 	s1 = 1;
-	s2 = 5;
+	s2 = 2;
 	System.out.println("---- Sigma: " + s1 + "  " + s2);
 
 	ccs = new Circuit[1];
 	if (circuit.equals("F2ET"))
-	    ccs[0] = new F2ET_Wplus2_Wplus2(sBitLen+cBitLen-2, s1, s2); // for testing F2ET
+	    ccs[0] = new F2ET_Wplus2_Wplus2(w, s1, s2); // for testing F2ET
 	else
-	    ccs[0] = new F2FT_2Wplus2_Wplus2((sBitLen+cBitLen-2)/2, s1, s2); // for testing F2FT
+	    ccs[0] = new F2FT_2Wplus2_Wplus2(w, s1, s2); // for testing F2FT
     }
 
     public static State execCircuit(BigInteger[] slbs, BigInteger[] clbs) throws Exception {
